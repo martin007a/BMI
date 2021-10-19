7c07b7ae511bd5325e40492639316829efa8cb3e
slavicek.martin <slaviekmartin@post.cz>
2021-10-19 11:36:23 +0200

    hotovo

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -6,7 +6,58 @@ namespace BMI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("jaj");
+            double vaha;
+            double vyska;
+            double výpočet;
+            while (true)
+            {
+
+                Console.Clear();
+                Console.WriteLine("Výpočet BMI");
+                Console.WriteLine("---------------------------------------");
+
+
+                Console.WriteLine("Zadej svou váhu (kg):  ");
+                double.TryParse(Console.ReadLine(), out vaha);
+                
+                if (vaha <= 5)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(" Nelze použít číslo menší než 5!!");
+                }
+                else
+                {
+                    Console.WriteLine("Zadej svou výšku (cm)  ");
+                    double.TryParse(Console.ReadLine(), out vyska);
+                    
+                    if (vyska <= 50) 
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Nelze použít číslo nenší než 50!!");
+
+                    }
+                    else
+                    {
+
+                        výpočet = (vaha) / (vyska / 100)*(vyska / 100);
+                        Console.WriteLine();
+                        Console.WriteLine($"Tvoje BMI je {výpočet}");
+
+                    } 
+
+                }
+                Console.ReadLine();
+          
+            
+            
+            
+            
+            
+            
+            
+            
+            }
         }
     }
+
 }
